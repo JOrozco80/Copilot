9315033f3ca8f02ccfa32b72f635da6c2a1bf8fe
JOrozco80 <juan.h.orozco@tutanota.com>
2022-06-19 16:21:41 -0400

    First test run of copilot.

---
+++ src/coPilot.java
@@ -0,0 +1,8 @@
+class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+//find all images without alternate text
+    //and give them a red border
+    void process(){}
+}
